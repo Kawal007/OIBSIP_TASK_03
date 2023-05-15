d13f9721c2bdc19608b52828a5a16c0a07c2fd07
Kawal Jot Singh <131584981+Kawal007@users.noreply.github.com>
2023-05-15 15:20:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var cel= document.getElementById("celcius")
+var fah= document.getElementById("Fahrenheit")
+
+cel.addEventListener('input',function(){
+    let c=this.value
+    let f=(c*9/5)+32
+
+    if(!Number.isInteger(f)){
+        f =f.toFixed(4)
+    }
+    fahrenheit.value=f;
+});
+fahren.addEventListener('input',function(){
+    let f =this.value
+    let c = (f-32)* 5/9;
+
+    if(!Number.isInteger(c)){
+        c =c.toFixed(4)
+    }
+    celcius.value=c;
+});
+
+
